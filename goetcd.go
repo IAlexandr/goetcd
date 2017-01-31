aaa0780311771993a9ed732946867d997fad4f93
Alexandr Ivanov <aivanov@informatica.ru>
2017-01-31 14:38:46 +0300

    DelKey, GetKeys

--- goetcd.go
+++ goetcd.go
@@ -33,10 +33,28 @@ func GetKey(route string) (string, error) {
 	}
 }
 
-func SetConf(url string) (client.KeysAPI, error) {
-	log.Printf("etcd connecting.. %q", url)
+func GetKeys(route string) (map[string]interface{}, error) {
+	res := make(map[string]interface{})
+
+	resp, err := kapi.Get(context.Background(), route, nil)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	} else {
+		log.Printf("Get is done. Metadata is %q\n", resp)
+		log.Printf("%q key has %q value\n", resp.Node.Key, resp.Node.Value)
+
+		for _, node := range resp.Node.Nodes {
+			res[node.Key] = node.Value//append(res[node.Key], node.Value)
+		}
+		return res, nil
+	}
+}
+
+func SetConf(url *string) (client.KeysAPI, error) {
+	log.Printf("etcd connecting.. %q", *url)
 	cfg := client.Config{
-		Endpoints:               []string{url},
+		Endpoints:               []string{*url},
 		Transport:               client.DefaultTransport,
 		// set timeout per request to fail fast when the target endpoint is unavailable
 		HeaderTimeoutPerRequest: time.Second,
@@ -47,4 +65,16 @@ func SetConf(url string) (client.KeysAPI, error) {
 	}
 	kapi = client.NewKeysAPI(c)
 	return kapi, err
+}
+
+func DelKey(route string) (error) {
+	resp, err := kapi.Delete(context.Background(), route, nil)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	} else {
+		log.Printf("Delete is done. Metadata is %q\n", resp)
+		log.Printf("%q key has %q value\n", resp.Node.Key, resp.Node.Value)
+		return nil
+	}
 }
